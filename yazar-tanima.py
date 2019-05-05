72df77c906702f18bfdc5a6c113d710832476f8d
furkanatesli <barbaros6034@gmail.com>
2019-05-05 23:42:38 +0300

    metin_düzenle fonksiyonu yazıldı
    metin_düzenle fonksiyonu yazıldı ancak kayıt formatı hatalı olduğu
    için düzenlenen metinlere farklı bir format gerekiyor ya da
    düzenlenen metinlerin ek düzenlemelere ihtiyacı var.

--- yazar-tanima.py
+++ yazar-tanima.py
@@ -1,19 +1,24 @@
 import os
 import re
-os.system('ac.bat')               
-##ana dizindeki ac.bat dosyası çalışarak "\egitim-verileri\duzensiz\" dizininde "yazarlar.txt" isminde, içerisinde 
-# duzensiz klasöründeki yazarlarin isimleri bulunan dosya oluşturuyor
-
-##"yazarlar.txt" içerisindeki verileri kullanışlı yala getirmek için dosya içerisindeki verileri düzenleyip "\egitim-verileri\duzenli\" dizinine 
-# "yazarlar.txt" olarak kayıt ediyoruz
+import codecs
+os.system('ac.bat')
 def isim_duzenle():
     dosya_oku = open("C:\Users\Furkan\Desktop\yazar-tanima\egitim-verileri\duzensiz\yazarlar.txt", "r").read()
-    yazarlar=re.split(".txt|yazarlar.txt|yazarlar.bat|yazarlar1.txt",dosya_oku)
+    yazarlar = re.split(".txt|yazarlar.txt|yazarlar.bat|yazarlar1.txt",dosya_oku)
     dosya_yaz = open("C:\Users\Furkan\Desktop\yazar-tanima\egitim-verileri\duzenli\yazarlar.txt", "w")
     for i in yazarlar:
       dosya_yaz.write(i.strip()+"\n")
       print(i.strip()+"\n")
     dosya_yaz.close()
 
-#def metin_duzenle();
-isim_duzenle()
+def metin_duzenle():
+    dosya_oku=open("C:\Users\Furkan\Desktop\yazar-tanima\egitim-verileri\duzenli\yazarlar.txt", "r")
+    yazarlar=dosya_oku.read().strip().split()
+    for yazar_adi in yazarlar:
+      yazar_dosya_oku = open("C:\\Users\\Furkan\\Desktop\\yazar-tanima\\egitim-verileri\\duzensiz\\"+yazar_adi+".txt", "r").read()
+      yazar_dosya_oku = yazar_dosya_oku.lower().decode('iso-8859-9')
+      yazar_dosya_oku = yazar_dosya_oku.encode("utf-8","ignore")
+      yazar_dosya_yaz = open("C:\\Users\\Furkan\\Desktop\\yazar-tanima\\egitim-verileri\\duzenli\\"+yazar_adi+".txt",  "w")
+      yazar_dosya_yaz.write(str(yazar_dosya_oku.split()))
+      yazar_dosya_yaz.close()
+metin_duzenle()
