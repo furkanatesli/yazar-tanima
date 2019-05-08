c9a9d0f41342758a2fdb8f48fbf11d324da26f6c
furkanatesli <barbaros6034@gmail.com>
2019-05-08 16:29:30 +0300

    metin_düzenle güncellendi kelime_say eklendi
    metin_düzenleme işlemlerindeki türkçe karakter problemi türkçe karakterleri ingilizce
    karakterlere çevrilerek giderildi
    kelime_say fonksiyonu eklendi

--- yazar-tanima.py
+++ yazar-tanima.py
@@ -1,7 +1,11 @@
+# -*- coding: utf-8 -*-
 import os
 import re
 import codecs
+import string
+
 os.system('ac.bat')
+
 def isim_duzenle():
     dosya_oku = open("C:\Users\Furkan\Desktop\yazar-tanima\egitim-verileri\duzensiz\yazarlar.txt", "r").read()
     yazarlar = re.split(".txt|yazarlar.txt|yazarlar.bat|yazarlar1.txt",dosya_oku)
@@ -16,9 +20,30 @@ def metin_duzenle():
     yazarlar=dosya_oku.read().strip().split()
     for yazar_adi in yazarlar:
       yazar_dosya_oku = open("C:\\Users\\Furkan\\Desktop\\yazar-tanima\\egitim-verileri\\duzensiz\\"+yazar_adi+".txt", "r").read()
-      yazar_dosya_oku = yazar_dosya_oku.lower().decode('iso-8859-9')
-      yazar_dosya_oku = yazar_dosya_oku.encode("utf-8","ignore")
+      yazar_dosya_oku = yazar_dosya_oku.lower()
+      yazar_dosya_oku = yazar_dosya_oku.replace("\xf6","o").replace("\xe7","c").replace("\xfd","i").replace("\xdc","u").replace("\xf0","g").replace("\xfc","u").replace("\xfe","s").replace("\xdd","i").replace("\xd6","o").replace("\x91","").replace("(","").replace(")","").replace("\x85","").replace("\xee","i")
+      yazar_dosya_oku = yazar_dosya_oku.replace(".","").replace(",","").replace("'","").replace('"','').replace("\x92","").replace("\x93","").replace("\x94","").replace("\xc7","c").replace("\xd0","g").replace("\xde","s").replace("?","").replace("\xe2","a").replace("!","").replace("\xc2","a").replace("\xfb","u")
       yazar_dosya_yaz = open("C:\\Users\\Furkan\\Desktop\\yazar-tanima\\egitim-verileri\\duzenli\\"+yazar_adi+".txt",  "w")
       yazar_dosya_yaz.write(str(yazar_dosya_oku.split()))
       yazar_dosya_yaz.close()
-metin_duzenle()
+
+def kelime_say():
+      dosya_oku=open("C:\Users\Furkan\Desktop\yazar-tanima\egitim-verileri\duzenli\yazarlar.txt", "r")
+      yazarlar=dosya_oku.read().strip().split()
+      for yazar_adi in yazarlar:
+            kelime_dizi_oku = open("C:\\Users\\Furkan\\Desktop\\yazar-tanima\\egitim-verileri\\duzenli\\"+yazar_adi+".txt", "r").read()
+            print("***********************************************"+yazar_adi+"*****************************************************")
+            kelime_dizi_oku = kelime_dizi_oku.replace("[","").replace("]","").replace("'","").replace(",","")
+            kelime_dizi_oku = kelime_dizi_oku.split()
+            kelime_sayisi = {}
+            for kelime in kelime_dizi_oku:
+                  if kelime in kelime_sayisi:
+                        kelime_sayisi[kelime] += 1
+                  else:
+                        kelime_sayisi[kelime] = 1
+            for sayi in kelime_sayisi.keys():
+                  print("%s %s" %(sayi,kelime_sayisi[sayi]))
+            
+            print("**************************************************************************************************************")
+
+kelime_say()
